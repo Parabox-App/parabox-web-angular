4f53999470f46cc6915cfc789c76072de418a260
ojhdt <ojhdtmail@gmail.com>
2023-02-04 16:42:29 +0800

    side navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,14 @@
 import { Component } from '@angular/core';
+import '@material/web/button/filled-button.js';
+import '@material/web/button/outlined-button.js';
+import '@material/web/iconbutton/standard-icon-button.js';
+import '@material/web/navigationbar/navigation-bar.js';
+import '@material/web/navigationdrawer/navigation-drawer.js';
+import '@material/web/navigationdrawer/navigation-drawer-modal.js';
+import '@material/web/navigationtab/navigation-tab.js';
+import '@material/web/checkbox/checkbox.js';
+import '@material/web/fab/fab-extended.js';
+import '@material/web/icon/icon.js';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +17,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'parabox-web-angular';
+  shouldOpenNav = false;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
-import { NgModule } from '@angular/core';
+import {CUSTOM_ELEMENTS_SCHEMA, NgModule} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatIconModule } from "@angular/material/icon";
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatSidenavModule,
+    BrowserAnimationsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppModule { }
