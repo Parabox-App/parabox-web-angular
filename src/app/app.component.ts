133e6bcbbc3a340cd22cf2eba62a6b73a32f4777
ojhdt <ojhdtmail@gmail.com>
2023-02-05 17:32:52 +0800

    subject style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,13 +9,40 @@ import '@material/web/navigationtab/navigation-tab.js';
 import '@material/web/checkbox/checkbox.js';
 import '@material/web/fab/fab-extended.js';
 import '@material/web/icon/icon.js';
+import '@material/web/list/list.js';
+import '@material/web/list/list-item.js';
+import '@material/web/ripple/ripple.js';
+
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { BreakpointObserver, BreakpointState } from '@angular/cdk/layout';
+import {MatDrawerMode} from "@angular/material/sidenav";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+  constructor(
+    private breakpointObserver: BreakpointObserver,
+  ) {
+    // detect screen size changes
+    this.breakpointObserver.observe([
+      "(max-width: 1024px)"
+    ]).subscribe((result: BreakpointState) => {
+      if (result.matches) {
+        this.navMode = "over"
+        this.defaultNavOpened = false
+      } else {
+        this.navMode = "side"
+        this.defaultNavOpened = true
+      }
+    });
+  }
   title = 'parabox-web-angular';
-  shouldOpenNav = false;
+  faGithub = faGithub
+
+  navMode: MatDrawerMode = "push"
+  defaultNavOpened = true
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,18 +7,24 @@ import { MatToolbarModule } from "@angular/material/toolbar";
 import { MatIconModule } from "@angular/material/icon";
 import { MatSidenavModule } from "@angular/material/sidenav";
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatRippleModule } from "@angular/material/core";
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { HoverClassDirective } from './hover-class.directive';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HoverClassDirective
   ],
   imports: [
     BrowserModule,
     MatToolbarModule,
     MatIconModule,
     MatSidenavModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatRippleModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent],
